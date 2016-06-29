9d20241382dba18db0de74bfe11d50b086efd416
lailashreth <lailashreth@gmail.com>
2016-06-29 13:10:33 +0300

    sos1

--- connectionToDevice/src/main/java/connectionToDevice/INTERFACE.java
+++ connectionToDevice/src/main/java/connectionToDevice/INTERFACE.java
@@ -2,7 +2,7 @@ package connectionToDevice;
 
 public interface INTERFACE {
 	
-	public boolean connect ();
+	public boolean connect (String host,int port);
 	String ip = "4.4.4.4";
 	String id="";
 	String version="";
--- connectionToDevice/src/main/java/connectionToDevice/Telnet.java
+++ connectionToDevice/src/main/java/connectionToDevice/Telnet.java
@@ -1,16 +1,27 @@
 package connectionToDevice;
 import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.PrintStream;
+import java.io.InputStream;
+import org.apache.commons.net.telnet.TelnetClient;
 
 public class Telnet implements INTERFACE {
 	
-	public boolean connect ()
+	public boolean connect (String host, int port)
 	{
-		Socket s = new Socket();
-		PrintWriter out_s = null;
-	    BufferedReader in_s = null;
-	    return false;
+		TelnetClient telnet = new TelnetClient();
+		
+		try {
+    		telnet.connect(host, 23);
+    		InputStream in = telnet.getInputStream();
+    		PrintStream out = new PrintStream(telnet.getOutputStream());
+    		return true;
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		
+	    
 
 	}
 	
