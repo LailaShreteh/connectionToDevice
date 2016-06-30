220c57a70b906902c0042d7100ecfcdd13a3eb02
reem11994 <reem112jazi@gmail.com>
2016-06-30 11:01:10 +0300

    SSH connection class "new modifications"

--- connectionToDevice/src/main/java/com/training/deviceoperation/deviceconnection/SSH.java
+++ connectionToDevice/src/main/java/com/training/deviceoperation/deviceconnection/SSH.java
@@ -3,6 +3,7 @@ package com.training.deviceoperation.deviceconnection;
 import java.io.File;
 import net.schmizz.sshj.SSHClient;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.io.*;
 import java.util.*;
 
@@ -16,26 +17,31 @@ public class SSH implements Connection {
 		
 		// create new SshParameters instance
 		SshParameters params = new SshParameters(hostname, port, username, password);
-		 
 		// create new SshHostKeys instance
 		SshHostKeys keys = new SshHostKeys();
 		  
-		SSHClient client=null;
+		SSHClient ssh=new SSHClient();
 		// specify valid remote server address
-		InetAddress address = InetAddress.getByName(hostname);
+		try {
+			InetAddress address = InetAddress.getByName(hostname);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		 
 		// add valid fingerprint to SshHostKeys instance
-		keys.addKey(address, "18:bc:ec:a5:0f:9a:fc:1a:60:96:7a:17:c8:ed:73:ac");
+		//keys.addKey(address, "18:bc:ec:a5:0f:9a:fc:1a:60:96:7a:17:c8:ed:73:ac");
 		 
 		// update SshParameters instance to validate against fingerprint in SshHostKeys instance
-		params.setHostKeys(keys, false);
+		//params.setHostKeys(keys, false);
 		 
 		// create new Ssh instance
-		SsH ssh = new SsH(params);
-		ssh.connect();
+		//SsH ssh = new SsH(params);
+		
+		//ssh.connect();
 		
 		// gets updated host keys (if updated)
-		keys = ssh.getHostKeys();
+		//keys = ssh.getHostKeys();
 	 return "";
 	}
 }
