2c622b14537ff54c7d6d0b2350c0cbeb42827848
USER <USER@192.168.204.33>
2016-07-28 08:45:58 +0300

    sos

--- connectionToDevice/src/main/java/com/training/deviceoperation/deviceconnection/JDBC/connectionToMySQL.java
+++ connectionToDevice/src/main/java/com/training/deviceoperation/deviceconnection/JDBC/connectionToMySQL.java
@@ -1,5 +1,6 @@
 package com.training.deviceoperation.deviceconnection.JDBC;
 
+import java.awt.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -17,12 +18,8 @@ public class connectionToMySQL {
 	static final String USER = "root";
 	static final String PASS = "laila";
 	static Connection conn = null;
-	
 
-
-
-	
-	public static void insert(EthernetProtocolEndpoint ePE) throws SQLException {
+	public static void insert(String table,String id ,List list) {
 		try {
 			Class.forName(JDBC_DRIVER);
 		} catch (ClassNotFoundException e) {
@@ -30,13 +27,14 @@ public class connectionToMySQL {
 			e.printStackTrace();
 		}
 		conn = DriverManager.getConnection(DB_URL, USER, PASS);
-		   Statement stmt = null;
-		   stmt = conn.createStatement();
-		   String sql = "INSERT INTO interface " +
-                   "VALUES ('"+ ePE.getName() +"' , '"+ ePE.getAdminStatus() +"', '"+ePE.getOperStatus()+"', "+ePE.getMtu()+", '"+ePE.getDuplexMode()+"', '"+ePE.getIfSpeed()+"', '"+ePE.getMacAddress()+"')";
-//		   String sql = "INSERT INTO interface VALUES ('reem', 'up' , 'down',1234,'full','1234kps','192.168.8.8' )";
-		   stmt.executeUpdate(sql);
-		   conn.close();
+		Statement stmt = null;
+		stmt = conn.createStatement();
+		String sql = "INSERT INTO " + table + "where "VALUES ('" + ePE.getName() + "' , '" + ePE.getAdminStatus() + "', '"
+				+ ePE.getOperStatus() + "', " + ePE.getMtu() + ", '" + ePE.getDuplexMode() + "', '" + ePE.getIfSpeed()
+				+ "', '" + ePE.getMacAddress() + "')";
+		
+		stmt.executeUpdate(sql);
+		conn.close();
 	}
 
 	public static void select(String TableName) {
@@ -53,33 +51,34 @@ public class connectionToMySQL {
 			e1.printStackTrace();
 		}
 		Statement stmt = null;
-		   try {
+		try {
 			stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM "+TableName);
-			 while ( rs.next() ) {
-	             String name = rs.getString("name");
-	             String adminStatus = rs.getString("adminStatus");
-	             String operationalStatus = rs.getString("operationalStatus");
-	             int MTU = rs.getInt("MTU");
-	             String duplexMode = rs.getString("duplex");
-	             String ifSpeed = rs.getString("speed");
-	             String macAddress = rs.getString("macAddress");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM " + TableName);
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String adminStatus = rs.getString("adminStatus");
+				String operationalStatus = rs.getString("operationalStatus");
+				int MTU = rs.getInt("MTU");
+				String duplexMode = rs.getString("duplex");
+				String ifSpeed = rs.getString("speed");
+				String macAddress = rs.getString("macAddress");
 
-	             System.out.println("**" + name + " || \t" + adminStatus + " || \t" + operationalStatus + " || \t" + MTU + " || \t" + duplexMode
-					+ " || \t" + ifSpeed + " || \t" + macAddress);
-	         }
+				System.out.println("**" + name + " || \t" + adminStatus + " || \t" + operationalStatus + " || \t" + MTU
+						+ " || \t" + duplexMode + " || \t" + ifSpeed + " || \t" + macAddress);
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}  
-		   try {
+		}
+		try {
 			conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public static void delete(String name,String TableName) {
+
+	public static void delete(String name, String TableName) {
 		Statement stmt = null;
 		try {
 			Class.forName(JDBC_DRIVER);
@@ -93,23 +92,24 @@ public class connectionToMySQL {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		   try {
+		try {
 			stmt = conn.createStatement();
-			 stmt.executeUpdate("DELETE FROM "+TableName+ "where name = '"+ name+"'" );
-			//select();
+			stmt.executeUpdate("DELETE FROM " + TableName + "where name = '" + name + "'");
+			// select();
 			System.out.println("\n");
-		   }catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		   try {
-				conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	public static void update(String name,String newName) {
+
+	public static void update(String name, String newName) {
 		try {
 			Class.forName(JDBC_DRIVER);
 		} catch (ClassNotFoundException e) {
@@ -123,21 +123,21 @@ public class connectionToMySQL {
 			e1.printStackTrace();
 		}
 		Statement stmt = null;
-		   try {
+		try {
 			stmt = conn.createStatement();
-			 stmt.executeUpdate("Update interface set name = '"+newName+"'"+ "where name = '"+ name+"'" );
-			//select();
+			stmt.executeUpdate("Update interface set name = '" + newName + "'" + "where name = '" + name + "'");
+			// select();
 			System.out.println("\n");
-		   }catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		   try {
-				conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
