d22e1bf82554305dc88828ddbcd32041d47305ee
lailashreth <lailashreth@gmail.com>
2016-07-28 15:11:09 +0300

    Class-map

--- connectionToDevice/src/main/java/com/training/deviceoperation/deviceconnection/model/class_map.java
+++ connectionToDevice/src/main/java/com/training/deviceoperation/deviceconnection/model/class_map.java
@@ -1,9 +1,48 @@
 package com.training.deviceoperation.deviceconnection.model;
 
-
 public class class_map {
 	private String className;
-	private String match_type;
-	
-}
+	private String classMapConfigurationMode;// match-all or match-any
+	private String description;
+	private String MatchType;// match access-group .......
 
+	public String getClassName() {
+		return className;
+	}
+
+	public class_map(String className, String classMapConfigurationMode, String description, String matchType) {
+		super();
+		this.className = className;
+		this.classMapConfigurationMode = classMapConfigurationMode;
+		this.description = description;
+		MatchType = matchType;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getClassMapConfigurationMode() {
+		return classMapConfigurationMode;
+	}
+
+	public void setClassMapConfigurationMode(String classMapConfigurationMode) {
+		this.classMapConfigurationMode = classMapConfigurationMode;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getMatchType() {
+		return MatchType;
+	}
+
+	public void setMatchType(String matchType) {
+		MatchType = matchType;
+	}
+}
--- connectionToDevice/src/main/java/com/training/deviceoperation/parser/SNMPParser.java
+++ connectionToDevice/src/main/java/com/training/deviceoperation/parser/SNMPParser.java
@@ -1,11 +1,20 @@
 package com.training.deviceoperation.parser;
 
+import java.util.List;
+
+import com.training.deviceoperation.deviceconnection.model.ACL;
 import com.training.deviceoperation.deviceconnection.model.EthernetProtocolEndpoint;
 
 public class SNMPParser implements Parser {
 
 	@Override
 	public EthernetProtocolEndpoint parsEthernetPE(String cmdBack) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ACL> parsACL(String cmd) {
 		// TODO Auto-generated method stub
 		return null;
 	}
