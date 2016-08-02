6400bddee19e3f31073c4e6dc0feb617e044c82b
reem11994 <reem112jazi@gmail.com>
2016-08-02 23:25:47 +0300

    ConnectionToDatabase Test

--- connectionToDevice/src/test/java/com/training/databasemanager/DataBaseConnectionTest.java
+++ connectionToDevice/src/test/java/com/training/databasemanager/DataBaseConnectionTest.java
@@ -1,13 +1,16 @@
 package com.training.databasemanager;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.training.databacemanager.DatabaseManager;
+import com.training.databacemanager.JDBC;
 import com.training.deviceoperation.deviceconnection.ConnectionFactory;
+import com.training.deviceoperation.deviceconnection.ConnectionRouter;
+import com.training.deviceoperation.deviceconnection.model.EthernetProtocolEndpoint;
 
 /**
  * 
@@ -15,26 +18,43 @@ import com.training.deviceoperation.deviceconnection.ConnectionFactory;
  *
  */
 public class DataBaseConnectionTest {
-	DatabaseManager conn = new 
-	@Before
+	ConnectionFactory connectionFactory = new ConnectionFactory();
+	private ConnectionRouter connection;
+	JDBC JDBCConnection=new JDBC();
+	boolean result = false;
+	String res=null;
+
+	/*@Before
 	public void setup() {
 
 		connection = ConnectionFactory.createConnection("TELNET");
 		connection.setHost("192.168.50.200");
 		connection.setPort(23);
 
-	}
+	}*/
 	@Test
 	public void testConnectionToDataBase() {
-		result = connection.connectToDevice();
-		connection.getInterface_Policy();
+		result = JDBCConnection.connectToDatabase();
+		res = connection.connectToDevice();
+		ArrayList<EthernetProtocolEndpoint> epeList = (ArrayList<EthernetProtocolEndpoint>) connection
+				.getEthernetPE();
+		//send data to dataBase
+		
+		for (int j = 0; j < epeList.size(); j++)
+		{
+			
+			JDBCConnection.insert(epeList.get(j), "interfaces");
+			//con.select();
+			System.out.println(epeList.get(j));
+		}
+		//connection.getInterface_Policy();
 
 	}
-	@AfterClass
-	public static void teardown() throws IOException {
+	/*@AfterClass
+	public  void teardown() throws IOException {
 		if (connection != null) {
 			connection.disconnectConnection();
 			connection = null;
 		}
-	}
+	}*/
 }
